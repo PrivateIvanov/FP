f84f51df96488d4de641d1ede8b439505d68ac7e
Kiril Ivanov <kirilivkiriliv@gmail.com>
2025-10-22 22:19:51 +0300

    Add main page and menu. Started js of main page.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+const menu = document.getElementsByClassName('menu');
+const characterScreen = document.getElementsByClassName('characterScreen');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+
+
 
 // Выбор страницы Волка
 function goToWolfPage() {
 window.location.href = 'wolfPage.html';
+}
+
+// Кнопки меню
+
+button.addEventListener('click', function() { 
+menu.style.display = 'none';
+
 }

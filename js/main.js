b4fea1f03c7c9bdb668da52c01fcf91221aa25a1
Kiril Ivanov <kirilivkiriliv@gmail.com>
2025-10-25 15:28:21 +0300

    add new main structure but need to remake it

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const menu = document.getElementsByClassName('menu');
-const characterScreen = document.getElementsByClassName('characterScreen');
+const menu = document.querySelector('.menu');
+const characterChooseWeather = document.querySelector('.characterChooseWeather');
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
@@ -13,7 +13,13 @@ window.location.href = 'wolfPage.html';
 
 // Кнопки меню
 
-button.addEventListener('click', function() { 
-menu.style.display = 'none';
+button1.addEventListener('click', function() { 
+menu.style.opacity = '0';
+menu.style.pointerEvents = 'none';
+menu.style.transition = 'opacity 0.3 ease';
 
-}
+
+    setTimeout(() => {
+        characterChooseWeather.style.opacity = '1';
+        characterChooseWeather.style.pointerEvents = 'auto';
+    }, 30 )});

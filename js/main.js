964501191e67c3c9676d5c1b174dfcd34947413b
Kiril Ivanov <kirilivkiriliv@gmail.com>
2025-10-25 17:16:16 +0300

    add menu animations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const menu = document.querySelector('.menu');
 const characterChooseWeather = document.querySelector('.characterChooseWeather');
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
-
+const button = {
+button1: document.getElementById('button1'),
+button2: document.getElementById('button2'),
+button3: document.getElementById('button3'),
+};
 
 
 // Выбор страницы Волка
@@ -14,12 +15,14 @@ window.location.href = 'wolfPage.html';
 // Кнопки меню
 
 button1.addEventListener('click', function() { 
-menu.style.opacity = '0';
-menu.style.pointerEvents = 'none';
-menu.style.transition = 'opacity 0.3 ease';
+menu.classList.add('moved-left');
+button.button1.style.paddingRight = '165px';
+button.button2.style.paddingRight = '42px';
+button.button3.style.paddingRight = '165px';
 
 
     setTimeout(() => {
         characterChooseWeather.style.opacity = '1';
         characterChooseWeather.style.pointerEvents = 'auto';
-    }, 30 )});
+        characterChooseWeather.style.transition = 'opacity 1.5s ease-in-out'; 
+    }, 2000 )});
